caf010f2f4ad3daacf706a23d0da3b7f3890acf9
JuaniGualtieri <juanigualtierics@gmail.com>
2025-09-25 16:10:31 -0300

    pieza ele

---
+++ tetris-poo-tdd-master-main/src/main/java/com/tetris/PieceLRight.java
@@ -0,0 +1,11 @@
+package com.tetris;
+
+public class PieceLRight extends Piece{
+    public PieceLRight() {
+        super(new int[][] {
+            {0, 1, 0},
+            {0, 1, 0},
+            {0, 1, 1}
+        });
+    }
+}
