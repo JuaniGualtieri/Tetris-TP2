60ca87549eadd8f4f2cea305c093f87f83d6801b
JuaniGualtieri <juanigualtierics@gmail.com>
2025-09-25 16:10:03 -0300

    perro

---
+++ tetris-poo-tdd-master-main/src/main/java/com/tetris/PieceDogRight.java
@@ -0,0 +1,13 @@
+package com.tetris;
+
+public class PieceDogRight extends Piece{
+    public PieceDogRight(){
+        super(new int[][] {
+            {0, 0, 0},
+            {0, 1, 1},
+            {1, 1, 0}
+        });
+    }   
+}
+
+
