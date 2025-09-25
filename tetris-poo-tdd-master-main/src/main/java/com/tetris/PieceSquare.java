ec308d712751772c9f619c41643dcff7401139c4
JuaniGualtieri <juanigualtierics@gmail.com>
2025-09-25 16:11:07 -0300

    square y stick

---
+++ tetris-poo-tdd-master-main/src/main/java/com/tetris/PieceSquare.java
@@ -0,0 +1,13 @@
+package com.tetris;
+
+public class PieceSquare extends Piece{
+    public PieceSquare() {
+        super(new int[][] {
+            {1, 1},
+            {1, 1}
+        });
+    }
+}
+
+
+
---
+++ tetris-poo-tdd-master-main/src/main/java/com/tetris/PieceStick.java
@@ -0,0 +1,12 @@
+package com.tetris;
+
+public class PieceStick extends Piece{
+    public PieceStick() {
+        super(new int[][] {
+            {1, 0, 0, 0},
+            {1, 0, 0, 0},
+            {1, 0, 0, 0},
+            {1, 0, 0, 0}
+        });
+    }
+}
