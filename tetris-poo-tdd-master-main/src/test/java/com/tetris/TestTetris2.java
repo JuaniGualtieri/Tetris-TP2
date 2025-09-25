4a647b0ca9324b82d452171dc4cd6bd042db1daa
JuaniGualtieri <juanigualtierics@gmail.com>
2025-09-25 16:17:09 -0300

    testeos

---
+++ tetris-poo-tdd-master-main/src/test/java/com/tetris/TestTetris2.java
@@ -0,0 +1,340 @@
+package com.tetris;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
+
+//import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestTetris2 {
+    @Test
+    public void girar_palo_izq() {
+        PieceStick pieceStick = new PieceStick();
+
+        pieceStick.rotateIzq();
+
+        int[][] formaEsperada = {
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {1, 1, 1, 1}
+        };
+
+        int[][] formaActual = pieceStick.getShape();
+
+        assertArrayEquals(formaEsperada, formaActual);
+
+    }
+
+    @Test
+    public void girar_palo_Der() {
+        PieceStick pieceStick = new PieceStick();
+
+        pieceStick.rotateDer();
+
+        int[][] formaEsperada = {
+            {1, 1, 1, 1},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
+
+        int[][] formaActual = pieceStick.getShape();
+
+        assertArrayEquals(formaEsperada, formaActual);
+    } 
+
+    @Test
+    public void girar_palo_Der_dos_veces() {
+        PieceStick pieceStick = new PieceStick();
+
+        pieceStick.rotateDer();
+        pieceStick.rotateDer();
+
+        int[][] formaEsperada = {
+            {0, 0, 0, 1},
+            {0, 0, 0, 1},
+            {0, 0, 0, 1},
+            {0, 0, 0, 1}
+        };
+
+        int[][] formaActual = pieceStick.getShape();
+
+        assertArrayEquals(formaEsperada, formaActual);
+    } 
+
+    @Test
+    public void girar_cubo_prueba() {
+        PieceSquare pieceSquare = new PieceSquare();
+
+        pieceSquare.rotateDer();
+
+        int[][] formaEsperada = {
+            {1, 1},
+            {1, 1}
+        };
+
+        int[][] formaActual = pieceSquare.getShape();
+
+        assertArrayEquals(formaEsperada, formaActual);
+    } 
+
+    @Test
+    public void girar_DogRight_derecha(){
+        PieceDogRight pieceDogRight = new PieceDogRight();
+
+        pieceDogRight.rotateDer();
+
+        int[][] formaEsperada = {
+            {1, 0, 0},
+            {1, 1, 0},
+            {0, 1, 0}
+        };
+
+        int[][] formaActual = pieceDogRight.getShape();
+
+        assertArrayEquals(formaActual, formaEsperada);
+    }
+
+    @Test
+    public void girar_DogRight_izquierda(){
+        PieceDogRight pieceDogRight = new PieceDogRight();
+
+        pieceDogRight.rotateIzq();
+
+        int[][] formaEsperada = {
+            {0, 1, 0},
+            {0, 1, 1},
+            {0, 0, 1}
+        };
+
+        int[][] formaActual = pieceDogRight.getShape();
+
+        assertArrayEquals(formaActual, formaEsperada);
+    }
+
+    @Test
+    public void girar_DogLeft_derecha(){
+        PieceDogLeft pieceDogLeft = new PieceDogLeft();
+
+        pieceDogLeft.rotateDer();
+
+        int[][] formaEsperada = {
+            {0, 1, 0},
+            {1, 1, 0},
+            {1, 0, 0}
+        };
+
+        int[][] formaActual = pieceDogLeft.getShape();
+
+        assertArrayEquals(formaActual, formaEsperada);
+    }
+
+    @Test
+    public void girar_DogLeft_izquierda(){
+        PieceDogLeft pieceDogLeft = new PieceDogLeft();
+
+        pieceDogLeft.rotateIzq();
+
+        int[][] formaEsperada = {
+            {0, 0, 1},
+            {0, 1, 1},
+            {0, 1, 0}
+        };
+
+        int[][] formaActual = pieceDogLeft.getShape();
+
+        assertArrayEquals(formaActual, formaEsperada);
+    }
+
+    @Test
+    public void girar_piezaT_derecha(){
+        PieceT pieceT = new PieceT();
+
+        pieceT.rotateDer();
+
+        int[][] formaEsperada = {
+            {1, 0, 0},
+            {1, 1, 0},
+            {1, 0, 0}
+        };
+
+        int [][] formaActual = pieceT.getShape();
+
+        assertArrayEquals(formaEsperada, formaActual);
+    }
+
+    @Test
+    public void girar_piezaT_izquierda(){
+        PieceT pieceT = new PieceT();
+
+        pieceT.rotateIzq();
+
+        int[][] formaEsperada = {
+            {0, 0, 1},
+            {0, 1, 1},
+            {0, 0, 1}
+        };
+
+        int [][] formaActual = pieceT.getShape();
+
+        assertArrayEquals(formaEsperada, formaActual);
+    }
+
+    @Test
+    public void girar_piezalLeft_derecha(){
+        PieceLLeft piecelLeft = new PieceLLeft();
+
+        piecelLeft.rotateDer();
+
+        int[][] formaEsperada = {
+            {1, 0, 0},
+            {1, 1, 1},
+            {0, 0, 0}
+        };
+
+        int [][] formaActual = piecelLeft.getShape();
+
+        assertArrayEquals(formaEsperada, formaActual);
+    }
+
+    @Test
+    public void girar_piezalLeft_izquierda(){
+        PieceLLeft piecelLeft = new PieceLLeft();
+
+        piecelLeft.rotateIzq();
+
+        int[][] formaEsperada = {
+            {0, 0, 0},
+            {1, 1, 1},
+            {0, 0, 1}
+        };
+
+        int [][] formaActual = piecelLeft.getShape();
+
+        assertArrayEquals(formaEsperada, formaActual);
+    }
+
+    @Test
+    public void girar_piezalDer_derecha(){
+        PieceLRight pieceLRight = new PieceLRight();
+
+        pieceLRight.rotateDer();
+
+        int[][] formaEsperada = {
+            {0, 0, 0},
+            {1, 1, 1},
+            {1, 0, 0}
+        };
+
+        int [][] formaActual = pieceLRight.getShape();
+
+        assertArrayEquals(formaEsperada, formaActual);
+    }
+
+    @Test
+    public void girar_piezalDer_izq(){
+        PieceLRight pieceLRight = new PieceLRight();
+
+        pieceLRight.rotateIzq();
+
+        int[][] formaEsperada = {
+            {0, 0, 1},
+            {1, 1, 1},
+            {0, 0, 0}
+        };
+
+        int [][] formaActual = pieceLRight.getShape();
+
+        assertArrayEquals(formaEsperada, formaActual);
+    }
+
+    @Test
+    public void girar_piezaT_random(){
+        PieceT pieceT = new PieceT();
+        Board board = new Board();
+
+        board.addPieceBoard(pieceT); 
+
+        pieceT.rotateRandom(); 
+
+        assertTrue(board.LimitesVeri(pieceT));
+    }
+
+    @Test
+    public void girar_piezaDogRight_random(){
+        PieceDogRight piece = new PieceDogRight();
+        Board board = new Board();
+
+        board.addPieceBoard(piece); 
+
+        piece.rotateRandom(); 
+
+        assertTrue(board.LimitesVeri(piece));
+    }
+
+    @Test
+    public void girar_piezaLleft_random(){
+        PieceLLeft piece = new PieceLLeft();
+        Board board = new Board();
+
+        board.addPieceBoard(piece); 
+
+        piece.rotateRandom(); 
+
+        assertTrue(board.LimitesVeri(piece));
+    }
+
+    @Test
+    public void girar_piezaLRight_random(){
+        PieceLRight piece = new PieceLRight();
+        Board board = new Board();
+
+        board.addPieceBoard(piece); 
+
+        piece.rotateRandom(); 
+
+        assertTrue(board.LimitesVeri(piece));
+    }
+
+    @Test
+    public void girar_piezaSquare_random(){
+        PieceSquare piece = new PieceSquare();
+        Board board = new Board();
+
+        board.addPieceBoard(piece); 
+
+        piece.rotateRandom(); 
+
+        assertTrue(board.LimitesVeri(piece));
+    }
+
+    @Test
+    public void girar_piezaStick_random(){
+        PieceStick piece = new PieceStick();
+        Board board = new Board();
+
+        board.addPieceBoard(piece); 
+
+        piece.rotateRandom(); 
+
+        assertTrue(board.LimitesVeri(piece));
+    }
+
+    @Test
+    public void girar_2_veces_piezaStick_random(){
+        PieceStick piece = new PieceStick();
+        Board board = new Board();
+
+        board.addPieceBoard(piece); 
+
+        piece.rotateRandom(); 
+        piece.rotateRandom(); 
+
+        assertTrue(board.LimitesVeri(piece));
+    }
+
+
+
+
+
+}
---
+++ tetris-poo-tdd-master-main/src/test/java/com/tetris/TestTetris3.java
@@ -0,0 +1,283 @@
+package com.tetris;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class TestTetris3 {
+    @Test
+    public void testBoard() {
+        Board board = new Board();
+        PieceStick piece = new PieceStick();
+        PieceT piece2 = new PieceT();
+        board.addPieceBoard(piece);
+        board.addPieceBoard(piece2);
+    }
+
+    @Test
+    public void testPieceStick_baja_una_vez() {
+        Board board = new Board();
+        PieceStick piece = new PieceStick();
+        board.addPieceBoard(piece); // Añade la pieza al tablero
+
+        piece.moveDown(); // Mueve la pieza hacia abajo
+
+        assertEquals(1, piece.getY()); // Verifica que la pieza ha bajado
+
+        // Verifica que la pieza está dentro de los límites del tablero
+        assertTrue(board.LimitesVeri(piece));
+    }
+
+    @Test
+    public void testClock() {
+        Board board = new Board();
+        PieceStick pieceStick = new PieceStick();
+        board.addPieceBoard(pieceStick);
+
+        int initialY = pieceStick.getY();
+
+        pieceStick.startTimer();
+
+        try {
+            // Espera 1 segundo para permitir solo un descenso
+            Thread.sleep(500); // Ajusta el valor si es necesario
+        } catch (InterruptedException e) {
+            
+        }
+
+        pieceStick.stopTimer();
+
+        // Verifica que la pieza solo haya descendido una o dos posiciones
+        assertEquals(initialY + 2, pieceStick.getY()); // Ajusta según la lógica
+    }
+
+    @Test
+    public void test_clock_pieceSquare(){
+        Board board = new Board();
+        PieceSquare pieceSquar = new PieceSquare();
+        board.addPieceBoard(pieceSquar);
+
+        assertEquals(0, pieceSquar.getY());
+
+
+        pieceSquar.startTimer();
+        try {
+            // Espera 1 segundo para permitir solo un descenso
+            Thread.sleep(100); // Ajusta el valor si es necesario
+        } catch (InterruptedException e) {
+            
+        }
+
+        pieceSquar.stopTimer();
+
+        // Verifica que la pieza solo haya descendido una o dos posiciones
+        assertEquals(2, pieceSquar.getY()); // Ajusta según la lógica
+    }
+
+    @Test
+    public void test_clock_pieceT(){
+        Board board = new Board();
+        PieceT piecet = new PieceT();
+        board.addPieceBoard(piecet);
+
+        assertEquals(0, piecet.getY());
+
+
+        piecet.startTimer();
+        try {
+            // Espera 1 segundo para permitir solo un descenso
+            Thread.sleep(100); // Ajusta el valor si es necesario
+        } catch (InterruptedException e) {
+            
+        }
+
+        piecet.stopTimer();
+
+        // Verifica que la pieza solo haya descendido una o dos posiciones
+        assertEquals(2, piecet.getY()); // Ajusta según la lógica
+    }
+
+    @Test
+    public void test_clock_pieceDog(){
+        Board board = new Board();
+        PieceDogLeft pieceDogL = new PieceDogLeft();
+        board.addPieceBoard(pieceDogL);
+
+        assertEquals(0, pieceDogL.getY());
+
+
+        pieceDogL.startTimer();
+        try {
+            Thread.sleep(100); 
+        } catch (InterruptedException e) {
+            
+        }
+
+        pieceDogL.stopTimer();
+
+        assertEquals(2, pieceDogL.getY()); 
+    }
+
+    @Test
+    public void test_clock_pieceL(){
+        Board board = new Board();
+        PieceLLeft pieceL = new PieceLLeft();
+        board.addPieceBoard(pieceL);
+
+        assertEquals(0, pieceL.getY());
+
+
+        pieceL.startTimer();
+        try {
+            // Espera 1 segundo para permitir solo un descenso
+            Thread.sleep(100); // Ajusta el valor si es necesario
+        } catch (InterruptedException e) {
+            
+        }
+
+        pieceL.stopTimer();
+
+        // Verifica que la pieza solo haya descendido una o dos posiciones
+        assertEquals(2, pieceL.getY()); // Ajusta según la lógica
+    }
+
+
+    
+    @Test
+    public void testPieceDog_Baja_dos_veces() {
+        Board board = new Board();
+        PieceDogRight piece = new PieceDogRight();
+        board.addPieceBoard(piece); 
+
+        piece.moveDown(); 
+
+        assertEquals(1, piece.getY()); 
+
+        assertTrue(board.LimitesVeri(piece));
+    }
+
+    @Test
+    public void testPieceStick_baja_dos_veces() {
+        Board board = new Board();          
+        PieceStick piece = new PieceStick(); 
+        board.addPieceBoard(piece);         
+
+        
+        piece.moveDown();                  
+        assertEquals(1, piece.getY());       
+        assertTrue(board.LimitesVeri(piece)); 
+
+        
+        piece.moveDown();                    
+        assertEquals(2, piece.getY());        
+        assertTrue(board.LimitesVeri(piece)); // Verifica que sigue dentro de los límites del tablero después del segundo movimiento
+    }
+
+    @Test
+    public void testPieceDog_baja_dos_veces() {
+        Board board = new Board();          
+        PieceDogRight piece = new PieceDogRight(); 
+        board.addPieceBoard(piece);         
+
+        
+        piece.moveDown();                  
+        assertEquals(1, piece.getY());       
+        assertTrue(board.LimitesVeri(piece)); 
+
+        
+        piece.moveDown();                    
+        assertEquals(2, piece.getY());        
+        assertTrue(board.LimitesVeri(piece)); // Verifica que sigue dentro de los límites del tablero después del segundo movimiento
+    }
+
+    @Test
+    public void testPieceDogLeft_baja_dos_vece() {
+        Board board = new Board();          
+        PieceDogLeft piecelef = new PieceDogLeft();
+    
+        board.addPieceBoard(piecelef);    
+
+        piecelef.moveDown();                  
+        assertEquals(1, piecelef.getY());       
+        assertTrue(board.LimitesVeri(piecelef)); 
+    }
+
+    @Test
+    public void testPieceDog_se_mueve_a_la_derecha() {
+        Board board = new Board();
+        PieceDogRight piece = new PieceDogRight();
+        board.addPieceBoard(piece); 
+
+        piece.moveRight(); 
+
+        assertEquals(1, piece.getX()); 
+
+        assertTrue(board.LimitesVeri(piece));
+    }
+ 
+    @Test
+    public void testPieceDog_se_mueve_a_la_izquierda() { 
+        Board board = new Board();
+        PieceDogRight piece = new PieceDogRight();
+        board.addPieceBoard(piece); 
+
+        piece.moveLeft(); 
+
+        assertEquals(-1, piece.getX()); 
+
+        assertFalse(board.LimitesVeri(piece));
+    }
+
+    @Test
+    public void testPieceDog_se_mueve_a_la_derecha_dos_veces_y_va_a_la_izquierda() { 
+        Board board = new Board();
+        PieceDogRight piece = new PieceDogRight();
+        board.addPieceBoard(piece); 
+
+        piece.moveRight(); 
+
+        assertEquals(1, piece.getX()); 
+
+        piece.moveRight(); 
+
+        assertEquals(2, piece.getX()); 
+
+        piece.moveLeft(); 
+
+        assertEquals(1, piece.getX()); 
+
+        assertTrue(board.LimitesVeri(piece));
+    }
+
+    @Test
+    public void testPieceDog_se_mueve_a_la_derecha_en_0_0() { 
+        Board board = new Board();
+        PieceDogRight piece = new PieceDogRight();
+        board.addPieceBoard(piece); 
+
+
+        piece.setX(0);
+        piece.moveRight(); 
+
+        assertEquals(1, piece.getX()); 
+
+        assertTrue(board.LimitesVeri(piece));
+    }
+
+    @Test
+    public void testPieceDog_se_mueve_a_la_izquierda_en_0_0() { 
+        Board board = new Board();
+        PieceDogRight piece = new PieceDogRight();
+        board.addPieceBoard(piece); 
+
+        piece.setX(0);
+
+        piece.moveLeft(); 
+
+        assertEquals(-1, piece.getX()); 
+
+        assertFalse(board.LimitesVeri(piece));
+    }
+
+
+}
