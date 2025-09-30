646409f3fc8612479a4715c841dbb69743964691
JuaniGualtieri <juanigualtierics@gmail.com>
2025-09-30 02:52:58 -0300

    test colision arreglo

--- tetris-poo-tdd-master-main/src/test/java/com/tetris/TestColision.java
+++ tetris-poo-tdd-master-main/src/test/java/com/tetris/TestColision.java
@@ -9,7 +9,7 @@ public class TestColision {
     public void colision_L_y_Stick() {
         Board board = new Board();
 
-        // 1) L cae hasta el fondo 
+        // L cae hasta el fondo 
         PieceLLeft pieceL = new PieceLLeft();
         pieceL.setX(0);
         pieceL.setY(0);
@@ -18,14 +18,14 @@ public class TestColision {
         int finalYL = pieceL.getY();
         assertEquals(board.grid.length - pieceL.getPiece().length, finalYL);
 
-        // 2) Ahora el stick horizontal cae y debe chocar con la L
+        // Ahora el stick horizontal cae y debe chocar con la L
         PieceStick stick = new PieceStick();
         stick.rotateDer();   // horizontal
         stick.setX(0);
         stick.setY(0);
         board.dropPiece(stick);
 
-        // 3) Verificaciones
+        // Verificaciones
         // Quedó por encima de la L
         assertTrue("El stick debe haber colisionado con la L", stick.getY() < finalYL);
 
